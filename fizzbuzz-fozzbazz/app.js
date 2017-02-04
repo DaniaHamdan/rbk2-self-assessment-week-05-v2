fd7a719f38b1ae1c477ee9f66df2437671655517
DaniaHamdan <dandonzh@gmail.com>
2017-02-04 10:05:45 +0200

    fizzBizz

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -18,16 +18,34 @@ angular.module('app', ['ngRoute'])
 /*  HINT: Make sure your controllers, methods, and variables 
     are named what $routeProvider and the partials are expecting  */
 
-  .factory('counter', function($scope){
+  .factory('counter', function($routeScope){
+    $routeScope.display="";
+    $routeScope.counter=0
 
   })
-  .controller('fizzbuzzCtrl', function($scope,counter){
+  .controller('fizzbuzzCtrl', function($routeScope,counter){
     var increment=function(){
-      
-    $scope.display=5
+      $routeScope.counter+=1;
+      if($routeScope.counter%3===0){
+        $routeScope.display="FIZZ"
+      }else if($routeScope.counter%5===0){
+        $routeScope.display="BIZZ"
+      }else if($routeScope.counter%3===0&&$routeScope.counter%5===0){
+        $routeScope.display="FIZZBIZZ"
+      }
+      return increment:increment
     }
-
   })
-  .controller('fozzbazzCtrl', function($scope,counter){
-
+  .controller('fozzbazzCtrl', function($routeScope,counter){
+var increment=function(){
+      $routeScope.counter+=1;
+      if($routeScope.counter%4===0){
+        $routeScope.display="FOZZ"
+      }else if($routeScope.counter%6===0){
+        $routeScope.display="BOZZ"
+      }else if($routeScope.counter%4===0&&$routeScope.counter%6===0){
+        $routeScope.display="FOZZBOZZ"
+      }
+      return increment:increment
+    }
   });
